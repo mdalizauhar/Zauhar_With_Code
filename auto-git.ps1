bfb0d100d399b0dc08d4ccb449af6f3ba0894a52
mdalizauhar <20dpcs007be@manuu.edu.in>
2026-09-10 00:56:45 +0530

    Auto update 2026-09-10 00:56:45

---
+++ auto-git.ps1
@@ -0,0 +1,36 @@
+$ProjectPath = "E:\Zauhar_With_Code"
+
+Set-Location $ProjectPath
+
+Write-Host "====================================="
+Write-Host " GitHub Auto Upload Started"
+Write-Host " Project: $ProjectPath"
+Write-Host "====================================="
+
+while ($true) {
+
+    $changes = git status --porcelain
+
+    if ($changes) {
+
+        Write-Host ""
+        Write-Host "Changes detected..."
+
+        git add .
+
+        $time = Get-Date -Format "yyyy-MM-dd HH:mm:ss"
+
+        git commit -m "Auto update $time"
+
+        git push origin main
+
+        if ($LASTEXITCODE -eq 0) {
+            Write-Host "Changes pushed to GitHub."
+        }
+        else {
+            Write-Host "GitHub push failed."
+        }
+    }
+
+    Start-Sleep -Seconds 5
+}
